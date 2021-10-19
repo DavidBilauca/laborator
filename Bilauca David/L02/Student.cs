f1c4144ab95cf3409f2c95f2ae55feb34598683e
DavidBilauca <79842701+DavidBilauca@users.noreply.github.com>
2021-10-19 15:36:18 +0300

    Add files via upload

---
+++ Bilauca David/L02/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String Nume { get; set; }
+        public String Prenume { get; set; }
+        public String Facultate { get; set; }
+        public char AnStudiu { get; set; }
+    }
+}
